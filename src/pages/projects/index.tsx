ff3bd50b502518c10ec47547cfd87cb02df7d80c
kazuyai <g2172026@tcu.ac.jp>
2025-02-21 18:43:15 +0900

    feat: Projectsページの仮作成

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <h1>Projectsページ</h1>
+        <p>ここにこれまでのプロジェクトの一覧を表示する</p>
+    </div>
+  )
+}
+
+export default index
