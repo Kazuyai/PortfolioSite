19c58a68ab8f2ea0070cddedd1f0e6c029baf2fe
kazuyai <g2172026@tcu.ac.jp>
2025-02-21 16:26:02 +0900

    feat: 簡易的なヘッダー・フッターの追加

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>My Portfolio Site</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/projects">Projects</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
