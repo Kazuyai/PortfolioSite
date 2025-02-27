255f729ba7435f0e7aab1a3bbc777220751ba0b8
kazuyai <g2172026@tcu.ac.jp>
2025-02-28 01:42:27 +0900

    feat: スクロールを促す矢印を追加

--- src/components/sections/Top.tsx
+++ src/components/sections/Top.tsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 import styles from "@/styles/sections/Top.module.scss";
 
 function Top() {
+  const topRef = useRef<HTMLDivElement>(null);
+  const [windowHeight, setWindowHeight] = useState(0);
+  const [topEndY, setTopEndY] = useState(0);
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    const rect = topRef.current?.getBoundingClientRect() as DOMRect;
+    setWindowHeight(window.innerHeight);
+    setTopEndY(rect?.bottom > 0 ? rect.bottom : 0);
+  }, [scrollY]);
+
   return (
-    <section className={styles.top}>
+    <section ref={topRef} className={styles.top}>
       <h1>Hello!</h1>
       <p>Welcome to my portfolio site</p>
+      <div 
+        className={styles.scroll}
+        style={{ 
+          opacity: topEndY / windowHeight,
+          display: 0 >= topEndY ? "none" : "flex"
+        }}
+      >
+        <img src="/images/arrow.svg" alt="arrow" />
+        <img src="/images/arrow.svg" alt="arrow" />
+        <img src="/images/arrow.svg" alt="arrow" />
+      </div>
     </section>
   );
 }
