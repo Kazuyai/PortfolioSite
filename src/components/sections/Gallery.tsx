33ba7942b495d1d687c5eadea6f0c85398d928e5
kazuyai <g2172026@tcu.ac.jp>
2025-03-02 00:47:12 +0900

    feat: Gallery部分のファイルを作成

---
+++ src/components/sections/Gallery.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '@/styles/sections/Gallery.module.scss'
+
+function Gallery() {
+  return (
+    <section className={styles.gallery}>
+        <h1>Gallery</h1>
+        <p>These are my gallery</p>
+    </section>
+  )
+}
+
+export default Gallery
