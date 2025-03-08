937f8e736c6f9d0ceb3e55856a1cf928f89a9c3c
kazuyai <g2172026@tcu.ac.jp>
2025-03-08 23:58:47 +0900

    feat: 各階にいるときにキャラクターが動けるようにした

--- src/components/utils/CharacterController.tsx
+++ src/components/utils/CharacterController.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { useSectionProgress } from "@/hooks/useSectionProgress";
 import { useFrame } from "@react-three/fiber";
@@ -15,22 +15,76 @@ const characterPositions = [
   { position: [0, -41.5, 0] },
 ];
 
-const CharacterController: React.FC<CharacterControllerProps> = ({
-  spacerRefs,
-  characterRef,
-}) => {
+const CharacterController: React.FC<CharacterControllerProps> = ({ spacerRefs, characterRef }) => {
   const { currentIndex, progress } = useSectionProgress(spacerRefs);
+  const [canMove, setCanMove] = useState(true);
+  const [move, setMove] = useState({
+    forward: false,
+    backward: false,
+    left: false,
+    right: false,
+  });
+
+  useEffect(() => {
+    setCanMove(false);
+    if (progress === 0) {
+      if (!characterRef.current) return;
+      if(characterPositions[currentIndex].position[1] - characterRef.current.position.y < 0.1) {
+        setCanMove(true);
+      }
+    }
+  }, [progress]);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (!canMove) return;
+      if (e.code === "KeyW") setMove((m) => ({ ...m, forward: true }));
+      if (e.code === "KeyS") setMove((m) => ({ ...m, backward: true }));
+      if (e.code === "KeyA") setMove((m) => ({ ...m, left: true }));
+      if (e.code === "KeyD") setMove((m) => ({ ...m, right: true }));
+    };
+    const handleKeyUp = (e: KeyboardEvent) => {
+      if (e.code === "KeyW") setMove((m) => ({ ...m, forward: false }));
+      if (e.code === "KeyS") setMove((m) => ({ ...m, backward: false }));
+      if (e.code === "KeyA") setMove((m) => ({ ...m, left: false }));
+      if (e.code === "KeyD") setMove((m) => ({ ...m, right: false }));
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [canMove]);
+
+  useEffect(() => {
+    if (!characterRef.current) return;
+    const charObj = characterRef.current;
+
+    if (move.forward) {
+      charObj.position.z -= 0.1;
+    }
+    if (move.backward) {
+      charObj.position.z += 0.1;
+    }
+    if (move.left) {
+      charObj.position.x -= 0.1;
+    }
+    if (move.right) {
+      charObj.position.x += 0.1;
+    }
+  }, [move]);
 
   useFrame(() => {
+    if (canMove) return;
+
     const charObj = characterRef.current;
     if (!charObj) return;
 
     const startIndex = currentIndex;
-    const endIndex =
-      currentIndex + 1 < characterPositions.length
-        ? currentIndex + 1
-        : currentIndex;
-    
+    const endIndex = currentIndex + 1 < characterPositions.length ? currentIndex + 1 : currentIndex;
+
     const startPos = characterPositions[startIndex].position;
     const endPos = characterPositions[endIndex].position;
 
