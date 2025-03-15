0aa76dd0db5098b93b7b7f631cdf31a825e5d24a
kazuyai <g2172026@tcu.ac.jp>
2025-03-15 23:29:41 +0900

    feat: Skillsセクションにスキルの項目を仮追加

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -18,7 +18,7 @@ const eventContent: ({ [key: string]: JSX.Element }) = {
   skills_event_1: (
     <div className={styles.eventContent}>
       <h2>Frontend Development</h2>
-      <p>React, TypeScript, Next.js</p>
+      <p>React, TypeScript, JavaScript, HTML, CSS, Sass, Next.js, PHP, WordPress</p>
     </div>
   )
 };
@@ -27,7 +27,20 @@ function Skills({ activeEvent }: SkillsProps) {
   return (
     <section className={styles.skills}>
       <h1>SKills</h1>
-      <p>These are my skills</p>
+      <div className={styles.skillsBox}>
+        <div className={styles.skillsBoxItem}>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+          <h2>TypeScript</h2>
+        </div>
+        <div className={styles.skillsBoxItem}>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
+          <h2>JavaScript</h2>
+        </div>
+        <div className={styles.skillsBoxItem}>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+          <h2>React</h2>
+        </div>
+      </div>
       {activeEvent && eventContent[activeEvent]}
     </section>
   );
