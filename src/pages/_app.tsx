6cd933dc1b5e9d4cabc0f3eb3bf24639752c462d
kazuyai <g2172026@tcu.ac.jp>
2025-03-24 13:14:08 +0900

    feat: Topページを開いた際に強制的にページの一番上に移動させる処理を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,16 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
   const isTopPage = router.pathname === "/";
+
+  useEffect(() => {
+    if (isTopPage) window.scrollTo(0, 0);
+  }, []);
+
   return isTopPage ? (
     <Component {...pageProps} />
   ) : (
