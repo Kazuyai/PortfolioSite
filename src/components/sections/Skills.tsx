808af81e7796bb5640e2a8ad752db8e1a6134b21
kazuyai <g2172026@tcu.ac.jp>
2025-03-29 01:38:04 +0900

    fix: skillsBoxItemのフェードインアニメ開始前に表示されていた問題を修正し、正しく下からフェードインするように調整

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -72,58 +72,6 @@ const Skills = ({ activeEvent }: SkillsProps) => {
             </div>
           ))
         }
-        {/* <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
-          <h2>TypeScript</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
-          <h2>JavaScript</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
-          <h2>React</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" />
-          <h2>Next.js</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/threejs/threejs-original.svg" />
-          <h2>Three.js</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
-          <h2>HTML</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg" />
-          <h2>Sass</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
-          <h2>CSS</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg" />
-          <h2>PHP</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" />
-          <h2>Python</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" />
-          <h2>C</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" />
-          <h2>C++</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg" />
-          <h2>Java</h2>
-        </div> */}
       </div>
 
       <h3 className={styles.skillsSubTitle}>Tools</h3>
@@ -136,34 +84,6 @@ const Skills = ({ activeEvent }: SkillsProps) => {
             </div>
           ))
         }
-        {/* <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" />
-          <h2>Git</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
-          <h2>GitHub</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/blender/blender-original.svg" />
-          <h2>Blender</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/unity/unity-original.svg" />
-          <h2>Unity</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/wordpress/wordpress-plain.svg" />
-          <h2>WordPress</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" />
-          <h2>Vercel</h2>
-        </div>
-        <div className={styles.skillsBoxItem}>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-plain.svg" />
-          <h2>Firebase</h2>
-        </div> */}
       </div>
       {activeEvent && eventContent[activeEvent]}
     </section>
