b6f5538d7472cdfa72ddb2828de6e63058b0fbcc
kazuyai <g2172026@tcu.ac.jp>
2025-04-04 17:37:37 +0900

    style: AboutセクションのテキストおよびGitHubリンクのスタイルを調整

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -13,9 +13,18 @@ const About = ({ activeEvent }: AboutProps) => {
     <section ref={ref} className={`${styles.about} ${isVisible ? styles.visible : ""}`}>
       <FadeinTitle title="About" isVisible={isVisible} />
       {/* <h2>Name</h2> */}
-      <p>岡村 和哉 (Kazuya Okamura)</p>
+      <p className={styles.name}>岡村 和哉 (Kazuya Okamura)</p>
       <p>東京都市大学大学院 環境情報学研究科 環境情報学専攻 修士1年</p>
       <p>趣味でWeb技術を活かしたコンテンツ制作に励んでいます。</p>
+      <div className={styles.links}>
+        <div className={styles.link}>
+          <a href="https.//github.com/Kazuyai" target="_blank" rel="noopener noreferrer">
+            <figure>
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" alt="GitHub" />
+            </figure>
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
