ba9beee2ab638ad143a063d40af2b983a2064c2d
kazuyai <g2172026@tcu.ac.jp>
2025-04-08 00:58:06 +0900

    fix: アンカーリンク付きURLでリロード時にページ最上部へ戻らないバグを修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,22 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
-  const isTopPage = router.pathname === "/";
+  // const isTopPage = router.asPath === "/";
+  const [isTopPage, setIsTopPage] = useState<boolean | null>(null);
 
   useEffect(() => {
-    if (isTopPage) window.scrollTo(0, 0);
+    setIsTopPage(router.asPath === "/");
+    if (router.asPath == "/") window.scrollTo(0, 0);
     if ('scrollRestoration' in window.history) {
       window.history.scrollRestoration = 'manual'
     }
   }, []);
+
+  if (isTopPage === null) return null;
 
   return isTopPage ? (
     <Component {...pageProps} />
