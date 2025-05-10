4657ec5cb7c0e6d71d77401a92d1b4a9e944ec5a
kazuyai <g2172026@tcu.ac.jp>
2025-05-10 21:45:36 +0900

    feat: スクロール位置に応じてヘッダーの背景を透過または白に切り替えるように変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,26 @@
 import styles from "@/styles/Header.module.scss";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [isTop, setIsTop] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 1) {
+        setIsTop(false);
+      } else {
+        setIsTop(true);
+      }
+    };
+
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isTop ? styles.isTop : ""}`}>
       <Link href="/" className={styles.icon}>
         <img src="./icon.png" alt=""/>
         <h1 className={styles.title}>Kazuya Okamura</h1>
