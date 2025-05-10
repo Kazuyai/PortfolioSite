df91d46ba9835726e8e517810cb222c6c0348f75
kazuyai <g2172026@tcu.ac.jp>
2025-05-10 22:20:05 +0900

    feat: mainVisualを設置

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import styles from '@/styles/pages/projects.module.scss'
 
 const index = () => {
   return (
-    <div>
-        <h1>Projectsページ</h1>
-        <p>ここにこれまでのプロジェクトの一覧を表示する</p>
+    <div className={styles.container}>
+      <div className={styles.mainVisual}>
+        <img src="./images/Gallery_01.png" alt="" />
+        <h2>Projects</h2>
+      </div>
+      <p>ここにこれまでのプロジェクトの一覧を表示する</p>
     </div>
   )
 }
