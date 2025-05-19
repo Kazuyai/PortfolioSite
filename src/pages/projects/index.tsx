77507d6c130e9952572cf4e54ea8b865c47b36a1
kazuyai <g2172026@tcu.ac.jp>
2025-05-20 01:14:58 +0900

    style: ポップアップのヘッダー部分のスタイルを調整

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styles from '@/styles/pages/projects.module.scss'
+import Link from 'next/link'
 
 const Index = () => {
   const [selectedCardId, setSelectedCardId] = useState<number | null>(null)
@@ -88,23 +89,28 @@ const Index = () => {
         <div className={styles.popup} onClick={() => setSelectedCardId(null)}>
           <div className={styles.popupExplain} onClick={(e) => e.stopPropagation()}>
             <div className={styles.popupExplainInner}>
-              <h3>タイトルがここに入る</h3>
-              <div className={styles.category}>
-                <span>Web</span>
+              <div className={styles.popupExplainHeader}>
+                <h3 className={styles.title}>タイトルがここに入る</h3>
+                <div className={styles.category}>
+                  <span>Web</span>
+                </div>
+                <div className={styles.tech}>
+                  <span>TypeScript</span>
+                  <span>React</span>
+                  <span>Next.js</span>
+                </div>
+                <div className={styles.link}>
+                  <Link href="#">link</Link>
+                </div>
+                <div className={styles.date}>
+                  <span>2025.05.15</span>
+                </div>
+                <div className={styles.imgContainer}>
+                  <img src="./images/Gallery_01.png" alt="" />
+                </div>
               </div>
-              <div className={styles.tech}>
-                <span>TypeScript</span>
-                <span>React</span>
-                <span>Next.js</span>
-              </div>
-              <div className={styles.date}>2025.05.15</div>
-              <div className={styles.img}>
-                <img src="./images/Gallery_01.png" alt="" />
-              </div>
-              <p>説明文がここに入る説明文がここに入る説明文がここに入る説明文がここに入る説明文がここに入る</p>
-              <div className={styles.link}>
-                <a href="#">GitHub</a>
-                <a href="#">URL</a>
+              <div className={styles.popupExplainBody}>
+                <p>説明文がここに入る説明文がここに入る説明文がここに入る説明文がここに入る説明文がここに入る</p>
               </div>
             </div>
           </div>
