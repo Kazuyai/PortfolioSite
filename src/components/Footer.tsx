9493cf382da3ab3679f19ffdfe6c9ea16af7dd67
kazuyai <g2172026@tcu.ac.jp>
2025-05-21 23:02:39 +0900

    feat: Footerの中身を作成

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,26 @@
+import Link from "next/link";
+import styles from "@/styles/Footer.module.scss";
+
 const Footer = () => {
   return (
-    <footer>
-      <p>My Portfolio Site</p>
+    <footer className={`${styles.footer}`}>
+      <Link href="/" className={styles.icon}>
+        <img src="./icon.png" alt=""/>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/gallery">Gallery</Link>
+          </li>
+        </ul>
+      </nav>
+      <p className={styles.copyright}>© Kazuya Okamura</p>
     </footer>
   );
 };
