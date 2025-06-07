68a4b55d1d31a800c26fc68dfd61c2a5c021e696
kazuyai <g2172026@tcu.ac.jp>
2025-06-07 15:27:16 +0900

    feat: 404ページの内容を作成

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import styles from "@/styles/pages/404.module.scss";
 
 const Custom404 = () => {
   return (
-    <div>ページが見つかりません！！</div>
+    <>
+    <div className={styles.container}>
+      <div className={styles.mainVisual}>
+        <div className={styles.description}>
+          <h2>404 Not Found</h2>
+          <p>
+            お探しのページは見つかりませんでした。<br />
+            URLが正しいか、ページが削除されていないかご確認ください。
+          </p>
+        </div>
+        <img src="./images/404.png" alt="404 Not Found" />
+      </div>
+    </div>
+    </>
   )
 }
 
